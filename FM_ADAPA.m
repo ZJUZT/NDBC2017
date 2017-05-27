43d29473d0e24b0232f8464575b7755b07742d53
ZJUZT <3120101111@zju.edu.cn>
2017-05-27 23:40:56 +0800

    basis done

--- FM_ADAPA.m
+++ FM_ADAPA.m
@@ -1,7 +1,8 @@
 rng('default')
 
-learning_rate = 1e-2;
-reg = 1e-4;
+C = 1e-1;
+eps = 0;
+delta = 1e-1;
 factor_num = 10;
 iter_num = 1;
 
@@ -9,8 +10,8 @@ test = test_data;
 num_train = size(train_data,1);
 num_test = size(test,1);
 
-MAE_SGD = zeros(iter_num, num_train);
-MAEtest_SGD = zeros(iter_num);
+MAE_ADAPA = zeros(iter_num, num_train);
+MAEtest_ADAPA = zeros(iter_num);
 % RMSE = zeros(1, num_train);
 
 num_user = max(train_data(:,1));
@@ -24,6 +25,9 @@ for i = 1:iter_num
     U = rand(num_user, factor_num);
     V = rand(num_item, factor_num);
     
+    U_ada = zeros(size(U,1),size(U,2));
+    V_ada = zeros(size(V,1),size(V,2));
+    
     for j = 1:num_train
         if mod(j,1e3)==0
             fprintf('%d iter---processing %dth sample\n', i, j);
@@ -34,13 +38,26 @@ for i = 1:iter_num
         r_hat = U(a,:)*(V(b,:)');
 
         if j == 1
-            MAE_SGD(i,j) = abs(r-r_hat);
+            MAE_ADAPA(i,j) = abs(r-r_hat);
         else
-            MAE_SGD(i,j) = (MAE_SGD(i,j - 1)*(j-1) + abs(r-r_hat)) / j;
+            MAE_ADAPA(i,j) = (MAE_ADAPA(i,j - 1)*(j-1) + abs(r-r_hat)) / j;
         end
+        
+        loss = max(abs(r-r_hat)-eps,0);
 
-        U(a,:) = U(a,:) - learning_rate * ((r_hat - r) * V(b,:) + 2 * reg * U(a,:)) ;
-        V(b,:) = V(b,:) - learning_rate * ((r_hat - r) * U(a,:) + 2 * reg * V(b,:));   
+        u_g = sign(r-r_hat)*(V(b,:));
+        U_ada(a,:) = U_ada(a,:) + u_g.*u_g;
+        v_g = sign(r-r_hat)*(U(a,:));
+        V_ada(b,:) = V_ada(b,:) + v_g.*v_g;
+        
+        V_A = delta+(V_ada(b,:));
+        U_A= delta+(U_ada(a,:));
+        
+        alpha_t3 = loss/(((V(b,:)./U_A*(V(b,:)')))+1/2/C);
+        beta_t3 = loss/(((U(a,:)./V_A*U(a,:)'))+1/2/C);  
+
+        U(a,:) = max(U(a,:) + sign(r-r_hat)*alpha_t3*V(b,:)./U_A,0);   
+        V(b,:) = max(V(b,:) + sign(r-r_hat)*beta_t3*U(a,:)./V_A,0);   
     end
     
     tic;
@@ -54,13 +71,13 @@ for i = 1:iter_num
     end
     toc;
     
-    MAEtest_SGD(i) = err/num_test;
+    MAEtest_ADAPA(i) = err/num_test;
     
 end
 
 %% plot
 % plot
-plot(MAE_SGD,'DisplayName','SFM-SGD');
+plot(MAE_ADAPA,'DisplayName','SFM-AdaPA');
 legend('-DynamicLegend');
 xlabel('Number of samples seen');
 ylabel('MAE'); 
